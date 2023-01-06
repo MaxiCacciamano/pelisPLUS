ee348f30d076725adf7d0b0341e64af6f7ec19bf
MaxiCacciamano <maximilianocacciamanomail.com>
2023-01-05 22:27:25 -0300

    ss

--- Api/src/Controls/userControler.js
+++ Api/src/Controls/userControler.js
@@ -1,6 +1,53 @@
 const axios  = require('axios')
+const user = require('../Schemas/user.schema')
+const {v4: uuidv4} = require('uuid')
+const { getToken } = require('../../jwt.config')
 
 const singUp = async(req,res)=>{
+    try{
+
+        //hacer midleware
+
+        //obtener la data del usuario
+
+        const {name, email} = req.body
+
+
+        //verificar que el usuario no exista la
+        let user = user.findOne({email: email}) || null
+
+        if(user !== null){
+            return res.json({
+                success:false,
+                msg:"Usuario ya existe"
+            })
+        }
+
+        //generar el codigo
+        const code = uuidv4();
+
+        //crear un nuevo usuario
+        user =  new user({name, email, code})
+
+        //generar un token
+        const token=getToken({name, email,code})
+
+
+        //obtener un templaate
+        
+
+        //enviar email
+    }
+    catch(err){
+        console.log(err,"soy el error de registro de ususario")
+        return res.json({
+            success: false,
+            msg: 'Error al registrar usuario'
+        })
+    }
+}
+
+const signin =async(req,res)=>{
 
 }
 
